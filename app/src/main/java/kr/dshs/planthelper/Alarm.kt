643cec94b385c8f75d3c216328e0c7a257143cc8
tmvkrpxl0 <ironman466@naver.com>
2022-09-15 21:00:20 +0900

    WIP....

--- app/src/main/java/kr/dshs/planthelper/Alarm.kt
+++ app/src/main/java/kr/dshs/planthelper/Alarm.kt
@@ -1,8 +1,15 @@
 package kr.dshs.planthelper
 
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.util.*
+import kotlin.random.Random
 
 /**
  * 24시간 포맷 사용함
@@ -19,4 +26,19 @@ fun setAlarm(alarmManager: AlarmManager, hour: Int, minute: Int, day: Int, pendi
     }
 
     alarmManager.setInexactRepeating(AlarmManager.RTC, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+}
+
+const val alarmChannel = "planthelper_by_tmvkrpxl0"
+
+class AlarmBroadcast: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        val builder = NotificationCompat.Builder(context, alarmChannel)
+            .setContentTitle("Alarm test")
+            .setContentText("Contents")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val manager = NotificationManagerCompat.from(context)
+        manager.notify(Random.nextInt(), builder.build())
+    }
+
 }
--- app/src/main/java/kr/dshs/planthelper/MainFragment.kt
+++ app/src/main/java/kr/dshs/planthelper/MainFragment.kt
@@ -1,13 +1,13 @@
 package kr.dshs.planthelper
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ListView
+import android.view.*
+import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kr.dshs.planthelper.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -41,6 +41,20 @@ class MainFragment : Fragment() {
         listView.setOnItemClickListener { parent: AdapterView<*>, clicked: View, position: Int, id: Long ->
             navController.navigate(R.id.PlantFragment)
         }
+
+        val addButton = view.findViewById<FloatingActionButton>(R.id.addplant)
+
+        addButton.setOnClickListener {
+            val popupView = layoutInflater.inflate(R.layout.add_plant_popup, null);
+
+            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+
+            val popupWindow = PopupWindow(popupView, width, height, true)
+
+            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+            popupWindow.dimBehind()
+        }
     }
 
 
@@ -48,4 +62,14 @@ class MainFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+fun PopupWindow.dimBehind() {
+    val container = contentView.rootView
+    val context = contentView.context
+    val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    val p = container.layoutParams as WindowManager.LayoutParams
+    p.flags = p.flags or WindowManager.LayoutParams.FLAG_DIM_BEHIND
+    p.dimAmount = 0.3f
+    wm.updateViewLayout(container, p)
 }
